8082e38a848721d6f7407de55846da0e3f2e82bb
Segun Konibire <segun.konibire@easeltv.com>
2019-08-26 20:47:38 +0100

    Displays all of recipe

---
+++ src/components/RecipeReader/RecipeReader.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const reciperReader = (props) => {
+
+    const { recipe: { name, ingredients, additionalContent }} = props;
+
+    return (
+        <div className={'reader'}>
+            <p><strong>As displayed on Product page</strong></p>
+            <hr />
+            <article className={'h-recipe'}>
+                <h4 className='p-name'>{name}</h4>
+                <ul>
+                    { ingredients.map((ingredient, idx) => {
+                        return ingredient.text.length ? <li key={idx} className='p-ingredient'>{ingredient.text}</li> : null
+                    })}
+                </ul>
+                { additionalContent.map((content, idx) => {
+                    const { text, body } = content;
+                    return (
+                        <div key={idx} 
+                            className='additional-content'>
+                                <h4>{ text }</h4>
+                                <p>{ body }</p>
+                        </div>
+                    )
+                })}
+            </article>
+        </div>
+    );
+}
+
+export default reciperReader;
