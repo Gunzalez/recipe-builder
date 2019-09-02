2113b0857c1047db027a35bb5c128f04af564d48
Segun Konibire <segun.konibire@easeltv.com>
2019-09-02 21:15:19 +0100

    Easier for multipurpose

--- src/components/AdditionalContent/AdditionalContent.js
+++ src/components/AdditionalContent/AdditionalContent.js
@@ -1,34 +1,15 @@
 import React from 'react';
 
-const additionalContent = ({ dataKey, title, body, removeContent }) => {
-
-    const titleId = `title-${dataKey}`;
-    const bodyId = `body-${dataKey}`;
+const additionalContent = ({ position, content }) => {
 
     return (
         <div className='form-group'>
             <div className='content-row'>
 
-                <label htmlFor={titleId}>Title</label>
-                <input type='text'
+                <textarea data-name={position}
                     className='form-control'
-                    data-name='title'
-                    defaultValue={title}
-                    id={titleId}
-                    data-key={dataKey} />
-
-                <label htmlFor={bodyId}>Content</label>
-                <textarea data-name='body'
-                    className='form-control'
-                    defaultValue={body}
-                    id={bodyId}
-                    data-key={dataKey}></textarea>
-
-                <button className='btn btn-default'
-                    onClick={removeContent}>
-                        <i className="material-icons">clear</i>
-                </button>
-
+                    placeholder='e.g. Intorduction to the recipe'
+                    defaultValue={content}></textarea>
             </div>
         </div>
     );
