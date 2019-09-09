25ea0f2dd7ad2f5b0f4564f181f522d3bfa894cc
Segun <gunzalez@hotmail.com>
2019-09-09 19:08:25 +0100

    Move remove button outside textarea

--- src/components/UI/ConfirmButton/ConfirmButton.js
+++ src/components/UI/ConfirmButton/ConfirmButton.js
@@ -2,13 +2,26 @@ import React from 'react';
 
 const confirmButton = ({ deleteState, removeIngredient, confirmRemove }) => {
 
-    return (
-        <button type='button' className={deleteState ? 'btn btn-default delete' : 'btn btn-default'}
-            onClick={ deleteState ? removeIngredient : confirmRemove }>
+    let display = (
+        <button type='button' className={'btn btn-default'}
+            onClick={ confirmRemove }>
                 <i className="material-icons">remove</i>
-                { deleteState ? <span>Delete?</span> : null }
         </button>
     )
+
+    if( deleteState ){
+        display = (
+            <div className={'btn-outer'}>
+                <button type='button' className={'btn btn-default delete'}
+                    onClick={ removeIngredient }>
+                        <i className="material-icons">remove</i>
+                </button>
+                <span>Delete?</span>
+            </div>
+        )
+    }
+
+    return display;
 }
 
 export default confirmButton;
